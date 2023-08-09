958d82f730423171ac4f97c9bb6fc8dd522c0bbe
cusmoritz <cus.moritz@gmail.com>
2023-08-09 16:51:04 -0600

    chore: adding src/App.js and src/index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import everything
+import React from "react";
+import { ReactDOM } from "react";
+import App from "./App";
+
+const app = ReactDom.createRoot(document.getElementById("app"));
+
+app.render(
+    <App />
+);
